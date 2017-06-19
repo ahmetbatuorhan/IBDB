baf16f8e02bb9df5716ff5850e2d45b2b46ce3fc
ahmetbatuorhan <aborhan96@hotmail.com>
2017-06-19 23:56:17 +0300

    Add files via upload

---
+++ constants/UIColors.java
@@ -0,0 +1,11 @@
+package constants;
+
+import java.awt.Color;
+
+public class UIColors 
+{
+    
+    public final static Color DARKBLUE = new Color(16, 96, 160);
+    public final static Color NIGHTBLUE = new Color(1, 46, 83);
+    
+}
